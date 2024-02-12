bff531672a3e8653a82761b02b1dfbdc4123ab91
Tariq <tariq9853@gmail.com>
2024-02-12 15:55:53 +0100

    Fixing my problems

--- src/connection_handler.rs
+++ src/connection_handler.rs
@@ -1,23 +1,31 @@
 use chrono::Utc;
-use uuid::Uuid;
-use std::{net::SocketAddr};
+use log::info;
+use std::{io::Cursor, net::SocketAddr};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    sync::{broadcast},
+    sync::broadcast,
 };
+use uuid::Uuid;
 
-use crate::{chatroom::Chatroom, commands::{Command, GreetCommand, RemoveUserByUuid}};
+use crate::{
+    chatroom::Chatroom,
+    commands::{Command, GreetCommand, RemoveUserByUuid},
+};
 
 pub async fn handle_connection(
     mut socket: tokio::net::TcpStream,
     addr: SocketAddr,
     tx: broadcast::Sender<(String, SocketAddr)>,
-    mut chatroom: Chatroom,
+    chatroom: Chatroom,
 ) {
     let (reader, mut writer) = socket.split();
     let mut reader: BufReader<tokio::net::tcp::ReadHalf<'_>> = BufReader::new(reader);
     let mut line: String = String::new();
     let mut rx: broadcast::Receiver<(String, SocketAddr)> = tx.subscribe();
+
+    let welcome_message = format!("{}\nWelcome to the chat server\n", Uuid::new_v4());
+    let mut buffer = Cursor::new(welcome_message);
+    let _ = writer.write_all_buf(&mut buffer).await;
 
     loop {
         tokio::select! {
@@ -26,30 +34,16 @@ pub async fn handle_connection(
                     break;
                 }
                 let trimmed_line = line.trim();
-                if trimmed_line.starts_with("/hello") {
-                    // Respond directly to the command without broadcasting
-                    writer.write_all("world\n".as_bytes()).await.unwrap();
-                    let greet_command = GreetCommand::new();
-                    greet_command.execute(&mut chatroom, "".to_string());
-                } else if trimmed_line.starts_with("/remove ") {
-                    // Extract the UUID from the command input
-                    let uuid_arg = trimmed_line.strip_prefix("/remove ").unwrap_or("").to_string();
-                    if let Ok(_) = Uuid::parse_str(&uuid_arg) {
-                        let remove_command = RemoveUserByUuid::new();
-                        remove_command.execute(&mut chatroom, uuid_arg);
-                        // Optional: send confirmation message to the admin/user who issued the command
-                        writer.write_all("User removed.\n".as_bytes()).await.unwrap();
-                    } else {
-                        writer.write_all("Invalid UUID format.\n".as_bytes()).await.unwrap();
-                    }
-                } else { 
+                if trimmed_line.starts_with("/") {
+                    parse_command(trimmed_line, chatroom.clone(), &mut writer).await;
+                }
+                else {
                     // Broadcast non-command messages
                     let timed_message = format!("{}: {}", Utc::now(), line);
                     tx.send((timed_message, addr)).unwrap();
                 }
                 line.clear();
             }
-
             result = rx.recv() => {
                 if let Ok((msg, other_addr)) = result {
                     // Send received messages to everyone except the sender of a command
@@ -62,3 +56,37 @@ pub async fn handle_connection(
     }
 }
 
+async fn parse_command(
+    line: &str,
+    mut chatroom: Chatroom,
+    writer: &mut tokio::net::tcp::WriteHalf<'_>,
+) {
+    if line.starts_with("/hello") {
+        // Respond directly to the command without broadcasting
+        writer.write_all("world\n".as_bytes()).await.unwrap();
+        let greet_command = GreetCommand::new();
+        greet_command.execute(&mut chatroom, "".to_string());
+    } else if line.starts_with("/remove ") {
+        // Extract the UUID from the command input
+        let uuid_arg = line.strip_prefix("/remove ").unwrap_or("").to_string();
+        if let Ok(_) = Uuid::parse_str(&uuid_arg) {
+            let remove_command = RemoveUserByUuid::new();
+            remove_command.execute(&mut chatroom, uuid_arg);
+            // Optional: send confirmation message to the admin/user who issued the command
+            writer
+                .write_all("User removed.\n".as_bytes())
+                .await
+                .unwrap();
+        } else {
+            writer
+                .write_all("Invalid UUID format.\n".as_bytes())
+                .await
+                .unwrap();
+        }
+    } else if line.starts_with("/connected"){
+        info!(
+            "{}({}) \n Current users: {:?}",
+            chatroom.id, chatroom.channel_name, chatroom.users
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use log::info;
-use std::{io::Cursor, sync::Arc};
-use tokio::{io::AsyncWriteExt, net::TcpListener, sync::{broadcast, Mutex}};
+use std::sync::Arc;
+use tokio::{net::TcpListener, sync::{broadcast, Mutex}};
 use uuid::Uuid;
 
 mod chatroom;
@@ -10,23 +10,20 @@ mod logging_settings;
 
 #[tokio::main]
 async fn main() {
+    let addr = "0.0.0.0:8080";
     logging_settings::setup_loggers();
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    let listener = TcpListener::bind(addr).await.unwrap();
     let (tx, _) = broadcast::channel(10);
     let chatroom = Arc::new(Mutex::new(chatroom::Chatroom::new("chatroom".to_string())));
     
+    info!("Server started on {:?}", addr);
+    info!("Chatrooom created with {:?}", chatroom);
+
     loop {
-        let (mut socket, addr) = listener.accept().await.unwrap();
-        let welcome_message = format!("{}\nWelcome to the chat server\n", Uuid::new_v4());
-        let mut buffer = Cursor::new(welcome_message);
-        let _ = socket.write_all_buf(&mut buffer).await;
-        
+        let (socket, addr) = listener.accept().await.unwrap();
         let mut chatroom: tokio::sync::MutexGuard<'_, chatroom::Chatroom> = chatroom.lock().await;
         let _: Uuid = chatroom.add_user(addr);
-        info!(
-            "{}({}) | Current users: {:?}",
-            chatroom.id, chatroom.channel_name, chatroom.users
-        );
         
 
         let tx = tx.clone();
